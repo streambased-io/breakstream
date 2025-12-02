101b8c1f702a5dedfb4d194a1c1f03178b525d3d
thomaskwscott <thomaskwscott@gmail.com>
2025-12-02 09:29:47 +0000

    added basic ksi test

--- bin/stop.sh
+++ bin/stop.sh
@@ -6,6 +6,10 @@ cd $SCRIPT_DIR/environment
 docker-compose stop
 docker-compose rm -f
 
+# have to stop background separately
+docker-compose stop shadowtraffic_background
+docker-compose rm -f shadowtraffic_background
+
 if [ -d "$SCRIPT_DIR/environment/shadowtraffic" ]
 then
   rm -rf $SCRIPT_DIR/environment/shadowtraffic
